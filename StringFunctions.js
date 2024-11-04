98dbaf3792448352acba04198fbea24f685e2608
chryswds <dybas.chrys@gmail.com>
2024-11-04 09:25:22 +0000

    some string functions

---
+++ StringFunctions.js
@@ -0,0 +1,13 @@
+// Start by manipulating a simple string
+
+let str = "Hello World";
+
+//Extract a substring from the string
+
+let substring = str.substring(0,5);
+
+let newstr = str.replace("World","Universe");
+
+let lowercase = str.toLowerCase();
+
+console.log(lowercase);
