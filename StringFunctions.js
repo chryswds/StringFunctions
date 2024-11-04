9bcd22ea7b9facc20a68c83e91c030baf184c7de
chryswds <dybas.chrys@gmail.com>
2024-11-04 09:41:20 +0000

    search and replace

--- StringFunctions.js
+++ StringFunctions.js
@@ -10,7 +10,26 @@ let newstr = str.replace("World","Universe");
 
 let lowercase = str.toLowerCase();
 
-console.log("Original string " + str);
-console.log("Substring " + substring)
-console.log("New String " + newstr);
-console.log("Lower case " + lowercase);
+console.log("Original string : " + str);
+console.log("Substring : " + substring)
+console.log("New String : " + newstr);
+console.log("Lower case : " + lowercase);
+
+
+
+
+
+//Search and replace
+
+
+const originalStory = `The quick brown fox jumps over the lazy dog
+The quick brown fox is known for its agility`
+
+console.log(originalStory);
+
+
+//Perform search and replace
+
+const manipulatedStory = originalStory.replace(/quick/g, "slow");
+
+console.log(manipulatedStory);
