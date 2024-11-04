b442c7c540948d37b7c6a8f2a00f7ce38ec3578c
chryswds <dybas.chrys@gmail.com>
2024-11-04 09:27:40 +0000

    printing

--- StringFunctions.js
+++ StringFunctions.js
@@ -4,10 +4,13 @@ let str = "Hello World";
 
 //Extract a substring from the string
 
-let substring = str.substring(0,5);
+let substring = str.substring(0,5); //Does not include the element at the last index
 
 let newstr = str.replace("World","Universe");
 
 let lowercase = str.toLowerCase();
 
-console.log(lowercase);
+console.log("Original string " + str);
+console.log("Substring " + substring)
+console.log("New String " + newstr);
+console.log("Lower case " + lowercase);
